3e0625626ddd3b2602a70228cbfec6ed95ca8450
gncao523 <141056672+gncao523@users.noreply.github.com>
2024-07-05 21:09:25 +0200

    backend setup

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8488aa3f461e5d8bf9815a8dc68655ad'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '174c07c8-b07b-4057-a8f2-03877165b603'
+};
